9ac1b45a9fbdece0b33a652b5ed44e030f4c9762
thefallenmaster <gauthambala2006.2@gmail.com>
2026-07-02 09:23:00 +0530

    Task 5 Completed

--- Task-5:(CONTACT-BOOK).py
+++ Task-5:(CONTACT-BOOK).py
@@ -1,8 +1,11 @@
 cbook=[["Gautham",8838431619,"gauthambala2006@gmail.com","hello"]]
 def display():
-    for i in cbook:
-        print("Name:",i[0],"Phone Number:",i[1],"Email:",i[2],"Address:",i[3])
-        print()
+    if len(cbook)==0:
+        print("No Contact")
+    else:
+        for i in cbook:
+            print("Name:",i[0],"Phone Number:",i[1],"Email:",i[2],"Address:",i[3])
+
 def addcontact():
     name=input("Enter Name:")
     phone=int(input("Enter Phone Number:"))
@@ -20,15 +23,16 @@ while True:
     elif choice==3:
         name=input("Enter Name:")
         for i in cbook:
-            if i[1]==name:
+            if i[0]==name:
                 print("Name:", i[0], "Phone Number:", i[1], "Email:", i[2], "Address:", i[3])
                 print("Found!")
+                break
         else:
             print("Invalid Choice")
     elif choice==4:
         name=input("Enter Name:")
         for i in cbook:
-            if i[1]==name:
+            if i[0]==name:
                 print("Name:", i[0], "Phone Number:", i[1], "Email:", i[2],"Address:", i[3])
                 i[0] = input("Enter Name:")
                 i[1] = int(input("Enter Phone Number:"))
@@ -40,8 +44,9 @@ while True:
     elif choice==5:
         name=input("Enter Name:")
         for i in cbook:
-            if i[1]==name:
+            if i[0]==name:
                 cbook.remove(i)
+                print("Deleted!")
                 break
         else:
             print("Invalid Choice")
